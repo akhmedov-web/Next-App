8fe2b0b9d23a40e145eaba767aae047520d7de84
akhmedov-web <ahmedovshohbaxt@gmail.com>
2023-03-30 20:47:23 +0500

    v2

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,23 @@
 import MovieCard from "./MovieCard";
-
 export default async function Home() {
-  const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`)
-  const res = await data.json();
+    const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`)
+    const res = await data.json();
+    
   return (
     <div className="grid-container py-3">
-      {res.results.map((item) =>
+      {res.results.length ? res.results.map((item) =>
         <MovieCard
           id={item.id}
           title={item.title}
           ranking={item.vote_average}
           photo={item.backdrop_path}
         />
-      )}
+      )
+        :
+        <div class="spinner-border" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+      }
     </div>
   )
 }
